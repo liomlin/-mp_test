df42c809739c41a33861bfc44eb5b64cf1e0fbdb
成都_杨国林 <1922651044@qq.com>
2020-11-27 17:49:07 +0800

    mp

---
+++ src/main/java/com/gtgui/mp/mapper/EmployeeMapper.java
@@ -0,0 +1,11 @@
+package com.gtgui.mp.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.gtgui.mp.beans.Employee;
+
+/**
+ *
+ */
+public interface EmployeeMapper extends BaseMapper<Employee>{
+
+}
